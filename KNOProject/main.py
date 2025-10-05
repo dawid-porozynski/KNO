5b7f2c33fa646264d8a6f6abcb64e6635ec69d48
dawid-porozynski <pora13234@gmail.com>
2025-10-05 20:41:03 +0200

    initial commit

--- KNOProject/main.py
+++ KNOProject/main.py
@@ -1,37 +1,61 @@
-import tensorflow as tf
-import numpy as np
+import argparse
 import os
-import PIL
-import PIL.Image
+import numpy as np
 import tensorflow as tf
-import tensorflow_datasets as tfds
+import matplotlib.pyplot as plt
+
 
 model_path = "model.keras"
-mnist = tf.keras.datasets.mnist
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
-model = tf.keras.models.Sequential(
-    [
-        tf.keras.layers.Flatten(input_shape=(28, 28)),
-        tf.keras.layers.Dense(128, activation="relu"),
-        tf.keras.layers.Dropout(0.2),
-        tf.keras.layers.Dense(10, activation="softmax"),
-    ]
-)
-model.compile(
-    optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
-)
-model.fit(x_train, y_train, epochs=5)  # użyj verbose=0 jeśli jest problem z konsolą
-model.evaluate(x_test, y_test)
 
-model.save(model_path)
+
+def train_model():
+    mnist = tf.keras.datasets.mnist
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+    model = tf.keras.models.Sequential(
+        [
+            tf.keras.layers.Flatten(input_shape=(28, 28)),
+            tf.keras.layers.Dense(128, activation="relu"),
+            tf.keras.layers.Dropout(0.2),
+            tf.keras.layers.Dense(10, activation="softmax"),
+        ]
+    )
+    model.compile(
+        optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
+    )
+    model.fit(x_train, y_train, epochs=5)  # użyj verbose=0 jeśli jest problem z konsolą
+    model.evaluate(x_test, y_test)
+
+    model.save(model_path)
+    return model
 
 
 def load_model():
     return tf.keras.models.load_model(model_path)
 
+
 def predict_image(model, image_path):
-    image = keras.utils.load_img(image_path)
-    input_arr = keras.utils.img_to_array(image)
+    image = tf.keras.utils.load_img(image_path)
+    input_arr = tf.keras.utils.img_to_array(image)
     input_arr = np.array([input_arr])  # Convert single image to a batch.
+
     predictions = model.predict(input_arr)
+    return predictions
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--image", type=str)
+    args = parser.parse_args()
+
+    if os.path.exists(model_path):
+        model = load_model()
+    else:
+        model = train_model()
+
+    if args.image:
+        predict_image(model, args.image)
+
+
+if __name__ == "__main__":
+    main()
