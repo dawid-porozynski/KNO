a959001037cc06c1912046357d30e42150e14784
dawid-porozynski <pora13234@gmail.com>
2026-02-09 20:12:39 +0100

    improve LSTM model with loss plotting

--- LAB8/main.py
+++ LAB8/main.py
@@ -8,10 +8,9 @@ EPOCHS = 10
 
 
 def create_dataset(dataset):
-    # Pomocnicza funkcja do tworzenia okien czasowych
     dataX, dataY = [], []
     for i in range(len(dataset) - LOOK_BACK):
-        a = dataset[i : (i + LOOK_BACK)]
+        a = dataset[i: (i + LOOK_BACK)]
         dataX.append(a)
         dataY.append(dataset[i + LOOK_BACK])
     return np.array(dataX), np.array(dataY)
@@ -26,11 +25,9 @@ def main():
     train_data = dataset[:train_size]
     test_data = dataset[train_size:]
 
-    # Przygotowanie danych pod LSTM
     trainX, trainY = create_dataset(train_data)
     testX, testY = create_dataset(test_data)
 
-    # LSTM wymaga: próbki, kroki, cechy
     trainX = np.reshape(trainX, (trainX.shape[0], LOOK_BACK, 1))
     testX = np.reshape(testX, (testX.shape[0], LOOK_BACK, 1))
 
@@ -42,29 +39,35 @@ def main():
         ]
     )
 
-    # Liczymy dane w pionie, predykcja od praw. linii sin
     model.compile(optimizer="adam", loss="mean_squared_error")
     model.summary()
 
-    model.fit(trainX, trainY, epochs=EPOCHS, batch_size=32, verbose=1)
+    history = model.fit(trainX, trainY, epochs=EPOCHS, batch_size=32, verbose=1)
 
     train_predict = model.predict(trainX)
     test_predict = model.predict(testX)
 
     plt.figure(figsize=(10, 6))
-
     plt.plot(x_axis, dataset, label="Oryginał", color="lightgray", linestyle="--")
 
-    train_x_range = x_axis[LOOK_BACK : len(train_predict) + LOOK_BACK]
+    train_x_range = x_axis[LOOK_BACK: len(train_predict) + LOOK_BACK]
     plt.plot(train_x_range, train_predict, label="Trening", color="blue")
 
     test_start = len(train_predict) + (LOOK_BACK * 2)
-    test_x_range = x_axis[test_start : test_start + len(test_predict)]
-
+    test_x_range = x_axis[test_start: test_start + len(test_predict)]
     plt.plot(test_x_range, test_predict, label="Predykcja (Test)", color="red")
 
     plt.title("Predykcja funkcji Sinus (LSTM)")
     plt.legend()
+
+    plt.figure(figsize=(10, 4))
+    plt.plot(history.history['loss'], label='Błąd (Loss)')
+    plt.title('Historia uczenia (Spadek błędu)')
+    plt.xlabel('Epoka')
+    plt.ylabel('MSE')
+    plt.legend()
+    plt.grid(True)
+
     plt.show()
 
 
